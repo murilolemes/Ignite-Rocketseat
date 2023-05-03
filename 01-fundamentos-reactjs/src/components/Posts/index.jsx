add38162554c35e0038d209f0aa6f38b61f51632
Murilo Lemes <murilo.lemes.mhl@gmail.com>
2023-05-03 12:42:08 -0300

    Validando formulário e realizando likes

--- 01-fundamentos-reactjs/src/components/Posts/index.jsx
+++ 01-fundamentos-reactjs/src/components/Posts/index.jsx
@@ -33,7 +33,12 @@ export function Posts({ author, content, publishedAt }) {
   }
 
   function handleNewCommentChange() {
+    event.target.setCustomValidity('')
     setNewCommentText(event.target.value);
+  }
+
+  function handleNewCommentInvalid() {
+    event.target.setCustomValidity('Esse campo é obrigatório!')
   }
 
   function deleteComment(commentToDelete) {
@@ -42,6 +47,8 @@ export function Posts({ author, content, publishedAt }) {
     })
     setComments(commentsWithoutDeletedOne);
   }
+
+  const isNewCommentEmpty = newCommentText.length === 0
 
   return (
     <article className={styles.post}>
@@ -76,10 +83,14 @@ export function Posts({ author, content, publishedAt }) {
           placeholder='Deixe um comentário'
           value={newCommentText}
           onChange={handleNewCommentChange}
+          onInvalid={handleNewCommentInvalid}
+          required
         />
 
         <footer>
-          <button type='submit'>Publicar</button>
+          <button type='submit' disabled={isNewCommentEmpty}>
+            Publicar
+          </button>
         </footer>
       </form>
 
