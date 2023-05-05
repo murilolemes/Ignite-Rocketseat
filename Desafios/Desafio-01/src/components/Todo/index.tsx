adb2cf4ffa7c9522884733654a952231a36a9db1
Murilo Lemes <murilo.lemes.mhl@gmail.com>
2023-05-05 16:48:53 -0300

    Iniciando Desafio 01 - Praticando os conceitos do reactjs

--- Desafios/Desafio-01/src/components/Todo/index.tsx
+++ Desafios/Desafio-01/src/components/Todo/index.tsx
@@ -1,20 +1,28 @@
+import { useState } from 'react';
 import { Trash } from 'phosphor-react';
 
-import checkbox from '../../assets/checkbox.svg';
-import checked from '../../assets/ckecked.svg';
-
-import styles from './styles.module.css'
+import styles from './styles.module.css';
 
 export function Todo() {
+  const [checked, setChecked] = useState(false);
+
+  function handleCheckbox() {
+    checked ? setChecked(false) : setChecked(true);
+  }
+
   return (
     <div className={styles.container}>
-      <button className={styles.checkbox}>
-        {/* <img src={checkbox} /> */}
-        <img src={checked} />
-      </button>
 
+      <button type='button' onClick={handleCheckbox} className={styles.checkbox}>
+        <input type="checkbox" checked={checked} className={styles.inputCheckbox} />
+        <span className={styles.checkmark}></span>
+      </button>
       <p>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
-      <button type='button' title='Deletar tarefa'>
+      <button
+        type='button'
+        className={styles.deleteTodo}
+        title='Deletar tarefa'
+      >
         <Trash size={14} />
       </button>
     </div>
