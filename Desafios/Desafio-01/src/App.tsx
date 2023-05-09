9d4c073ab5e9951b9c3dafb073afd5547ccf9eb0
Murilo Lemes <murilo.lemes.mhl@gmail.com>
2023-05-08 21:45:43 -0300

    Removendo todo list

--- Desafios/Desafio-01/src/App.tsx
+++ Desafios/Desafio-01/src/App.tsx
@@ -1,13 +1,49 @@
+import { useState } from 'react';
 import { PlusCircle, ClipboardText } from 'phosphor-react';
 
 import { Header } from './components/Header'
 import { Input } from './components/Input'
-import { Todo } from './components/Todo';
+import { Todo, TodoProps } from './components/Todo';
 
 import styles from './app.module.css'
 import './styles/global.css'
 
+const todoList: TodoProps[] = [
+  {
+    id: 1,
+    content: 'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
+  },
+  {
+    id: 2,
+    content: 'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
+  },
+  {
+    id: 3,
+    content: 'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
+  },
+  {
+    id: 4,
+    content: 'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
+  },
+  {
+    id: 5,
+    content: 'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
+  },
+  {
+    id: 6,
+    content: 'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
+  }
+]
+
 export function App() {
+  const [updateTodoList, setUpdateTodoList] = useState([...todoList])
+
+  function handleDeleteTodo(id: number) {
+    const teste = updateTodoList.filter(todo => todo.id !== id)
+
+    setUpdateTodoList(teste)
+  }
+
   return (
     <div className={styles.container}>
       <Header />
@@ -20,21 +56,32 @@ export function App() {
       </form>
       <div className={styles.content}>
         <div className={styles.todoCount}>
-          <p className={styles.taskCreated}>Tarefas criadas <span>0</span></p>
-          <p className={styles.completed}>Concluídas <span>2 de 5</span></p>
+          <p className={styles.taskCreated}>
+            Tarefas criadas
+            <span>{updateTodoList.length}</span>
+          </p>
+          <p className={styles.completed}>
+            Concluídas
+            <span>2 de {updateTodoList.length}</span>
+          </p>
         </div>
         <div>
-          <div className={styles.notExistsTodoList}>
-            <ClipboardText size={56} />
-            <p className={styles.FirstP}>Você ainda não tem tarefas cadastradas</p>
-            <p>Crie tarefas e organize seus itens a fazer</p>
-          </div>
-
-          <Todo />
-          <Todo />
-          <Todo />
-          <Todo />
-          <Todo />
+          {updateTodoList.length === 0 ?
+            <div className={styles.notExistsTodoList}>
+              <ClipboardText size={56} />
+              <p className={styles.FirstP}>Você ainda não tem tarefas cadastradas</p>
+              <p>Crie tarefas e organize seus itens a fazer</p>
+            </div>
+            :
+            updateTodoList.map(todo => {
+              return (
+                <Todo key={todo.id}
+                  onHandleDeleteTodo={handleDeleteTodo}
+                  todoContent={todo}
+                />
+              )
+            })
+          }
         </div>
       </div>
     </div>
--- Desafios/Desafio-01/src/components/Todo/index.tsx
+++ Desafios/Desafio-01/src/components/Todo/index.tsx
@@ -3,25 +3,42 @@ import { Trash } from 'phosphor-react';
 
 import styles from './styles.module.css';
 
-export function Todo() {
+export interface TodoProps {
+  id: number;
+  content: string;
+}
+
+interface TodoContent {
+  todoContent: TodoProps;
+  onHandleDeleteTodo: (id: number) => void;
+}
+
+export function Todo({ todoContent, onHandleDeleteTodo }: TodoContent) {
   const [checked, setChecked] = useState(false);
 
   function handleCheckbox() {
     checked ? setChecked(false) : setChecked(true);
   }
 
+  function handleDeleteTodo(id: number) {
+    onHandleDeleteTodo(id);
+  }
+
   return (
     <div className={styles.container}>
 
       <button type='button' onClick={handleCheckbox} className={styles.checkbox}>
-        <input type="checkbox" checked={checked} className={styles.inputCheckbox} />
+        <input type="checkbox" onChange={handleCheckbox} checked={checked} className={styles.inputCheckbox} />
         <span className={styles.checkmark}></span>
       </button>
-      <p>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
+      <p className={checked ? styles.todoChecked : ''}>
+        {todoContent.content}
+      </p>
       <button
         type='button'
         className={styles.deleteTodo}
         title='Deletar tarefa'
+        onClick={() => handleDeleteTodo(todoContent.id)}
       >
         <Trash size={14} />
       </button>
