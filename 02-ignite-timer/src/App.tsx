cdf5e913b5394b0719f48c1a0a903d602e6f3bec
Murilo Lemes <murilo.lemes.mhl@gmail.com>
2023-05-16 15:13:04 -0300

    Definição de Rotas, criação e estilização das páginas

--- 02-ignite-timer/src/App.tsx
+++ 02-ignite-timer/src/App.tsx
@@ -1,12 +1,16 @@
+import { BrowserRouter } from 'react-router-dom'
+
+import { Router } from './Router'
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
-
 import { GlobalStyle } from './styles/global'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Hello World</h1>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
