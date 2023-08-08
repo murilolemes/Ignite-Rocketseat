8b00243eab67ff0f3363b6f526467a0c8872b3bc
Murilo Lemes <murilo.lemes.mhl@gmail.com>
2023-08-08 11:27:05 -0300

    Global style tweaks

--- Desafios/Desafio-03/src/styles/global.ts
+++ Desafios/Desafio-03/src/styles/global.ts
@@ -1,6 +1,15 @@
 import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
+  :root {
+    --title-l: 700 1.5rem/1.3 'Nunito';
+    --title-m: 700 1.25rem/1.6 'Nunito';
+    --title-s: 700 1.125rem/1.6 'Nunito';
+    --text-m: 400 1rem/1.6 'Nunito';
+    --text-s: 400 0.875rem/1.6 'Nunito';
+    --link: 400 0.75rem/1.6 'Nunito';
+  }
+
   * {
     margin: 0;
     padding: 0;
@@ -19,7 +28,7 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   body, input, textarea, button {
-    font: 400 1rem Nunito, sans-serif;
+    font: 400 1rem/1.6 'Nunito', sans-serif;
   }
 
   button {
